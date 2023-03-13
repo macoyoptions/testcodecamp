e063b06cccd6a7137863686fa16fd1e3f60c614b
macoyoptions <macoyoptions@gmail.com>
2023-03-13 12:18:36 -0700

    FirstJavaProjectTask14

---
+++ FirstJavaProjectTask14/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("task14, Print out several lines on your console");
+        System.out.println("Hello world");
+        System.out.println("Hello world, this is my first project");
+        System.out.println("Hello world, my name is Oshodi Monsurat Chinyere");
+        System.out.println("based in Ondo state,Nigeria");
+    }
+}
